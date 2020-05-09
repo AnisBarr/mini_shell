b2957e4927e6dc9afaec53f5d97271572ed75065
AnisBarr <barrma@hotmail.fr>
2020-05-09 19:00:00 +0200

    ajout de quelque fonctionalite

---
+++ readme.md
@@ -0,0 +1,15 @@
+
+1- les références a pointer sur une variable 
+
+2- move copy clone
+
+3- Rust est compilé nativement
+
+4- Un processus est un ensemble d'activités corrélées ou en interaction qui utilise des éléments d'entrée pour produire un résultat escompté, selon la norme ISO 9000:2015. Ces éléments sont soit des objets matériels soit des informations, soit les deux. (source : wiki)
+
+5- Que fait votre programme pendant que son enfant s’exécute ?
+Il attend la fin pour poir determiner le status 
+
+7- Les tubes sont un mécanisme de communication qui permet de réaliser des communications entre processus sous forme d'un flot continu d'octets. Les tubes sont un des éléments de l'agrément d'utilisation d'UNIX. C'est ce mécanisme qui permet l'approche filtre de la conception sous UNIX. UNIVERSITE marne la vallée
+
+10-L'identifiant de processus ou PID (Process IDentifier) est un code unique attribué sur les systèmes Unix ou Windows à tout processus lors de son démarrage. wiki
--- src/main.rs
+++ src/main.rs
@@ -8,26 +8,41 @@ use std::collections::HashMap;
 
 
 
-
+#[warn(unused_variables)]
 fn cmd_prossing(cmd : String){
-    let list_cmd = space_split(cmd.as_str());
+    let mut list_cmd = space_split(cmd.as_str());
 
     let filtered_env : HashMap<String, String> =  env::vars().filter(|&(ref k, _)| k == "TERM" || k == "TZ" || k == "LANG" || k == "PATH").collect();
+
+    if cmd.as_str().contains("|"){
+
+        let  list_cmd_splited :  Vec<String> = cmd.split("|").map(|x| x.to_string()).collect();
+        let  list_cmd_1 = space_split(list_cmd_splited[0].as_str());
+        let  list_cmd_2 = space_split(list_cmd_splited[1].as_str());
+
+        exec_pipe(list_cmd_1,list_cmd_2, &filtered_env);
+    }
 
     if list_cmd[0].as_str() == "cd" {
         cd(list_cmd);
     }
-    if list_cmd[0].as_str() == "back" {
+    else if list_cmd[0].as_str() == "back" {
         list_cmd.remove(0);
         exec_cmd_back(list_cmd,&filtered_env);
     }
     else {
         exec_cmd(list_cmd,&filtered_env);
     }
-    // else {
-    //     exec_cmd(list_cmd,&filtered_env);
-    // }
+
 }
+
+
+fn exec_pipe (_list_cmd_1 : Vec<String> , _list_cmd_2 : Vec<String> , _env : &HashMap<String, String>)  {
+    
+    // je sais pas comment le fair mais voila l'idée je prends les 2 cmd et l'env et c'est la que je fait de la magie 
+}
+
+
 
 fn space_split(user_input: &str) -> Vec<String> {
     let list_cmd_splited: Vec<_> = user_input.split_whitespace().map(|x| x.to_string()).collect();
@@ -35,8 +50,8 @@ fn space_split(user_input: &str) -> Vec<String> {
 }
 
 
-#[warn(dead_code)]
-fn exec_cmd(list_cmd : Vec<String> , env : &HashMap<String, String>) -> i32 {
+
+fn exec_cmd(list_cmd : Vec<String> , env : &HashMap<String, String>) -> std::process::ExitStatus {
     let  cmd = Command::new(&list_cmd[0])
         .args(&list_cmd[1..])
         .env_clear()
@@ -45,32 +60,32 @@ fn exec_cmd(list_cmd : Vec<String> , env : &HashMap<String, String>) -> i32 {
         .expect("erreur");
     
     println!("process exited with: {}", cmd);
-    1
+    cmd
 }
 
 //cette function exuecute des cmd en back end 
-fn exec_cmd_back(list_cmd : Vec<String> , env : &HashMap<String, String>)   {
-    let mut cmd = Command::new(&list_cmd[0])
+fn exec_cmd_back(list_cmd : Vec<String> , env : &HashMap<String, String>)   -> std::process::Child {
+    let  cmd = Command::new(&list_cmd[0])
         .args(&list_cmd[1..])
-        // .status()
         .env_clear()
         .envs(env)
         .spawn()
         .expect("erreur");
 
-    match cmd.try_wait() {
-            Ok(Some(status)) => println!("exited with: {}", status),
-            Ok(None) => {
-                println!("status not ready yet, let's really wait");
-                let res = cmd.wait();
-                println!("result: {:?}", res);
-            }
-            Err(e) => println!("error attempting to wait: {}", e),
-        }
+    // match cmd.try_wait() {
+    //         Ok(Some(status)) => println!("exited with: {}", status),
+    //         Ok(None) => {
+    //             println!("status not ready yet, let's really wait");
+    //             let res = cmd.wait();
+    //             println!("result: {:?}", res);
+    //         }
+    //         Err(e) => println!("error attempting to wait: {}", e),
+    //     }
 
-    // println!("process exited with: {}", cmd);
     println!("pid: {}",cmd.id());
+    cmd
 }
+
 
 // cette function chnage de ripo 
 fn cd(list_cmd : Vec<String>) {
